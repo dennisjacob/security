d450d0bb6d2324fe2604515dbbcb5aec7641e2ce
Dennis Jacob <34237923+dennisjacob@users.noreply.github.com>
2022-04-09 13:42:32 +0800

    Update generate-cert-signed-by-ca.sh

--- generate-cert-signed-by-ca.sh
+++ generate-cert-signed-by-ca.sh
@@ -37,4 +37,12 @@ EOF
 
 openssl req -new -key ${CERT_PREFIX}.key -out ${CERT_PREFIX}.csr -sha256  -config ./cert-config.conf
 openssl x509 -req -in ${CERT_PREFIX}.csr -CA $CA_PVT_CERT -CAkey $CA_PVT_KEY -days 365 -sha256 -out ${CERT_PREFIX}.crt -CAcreateserial -CAserial serial -extensions v3_req -extfile ./cert-config.conf
+echo 
+echo "Certificate generated..."
 openssl x509 -in  ${CERT_PREFIX}.crt -noout -subject -issuer -dates
+echo 
+echo "Private key - $(pwd)/${CERT_PREFIX}.key"
+echo "Private key - $(pwd)/${CERT_PREFIX}.crt"
+rm -f ./cert-config.conf serial $(pwd)/${CERT_PREFIX}.csr
+
+
